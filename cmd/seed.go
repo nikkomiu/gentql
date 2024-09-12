2037f26cb72de0c8c069ef54b2fd367326fb29a8
Nikko Miu <nikkoamiu@gmail.com>
2024-09-12 18:26:32 +0000

    Add seed

---
+++ cmd/seed.go
@@ -0,0 +1,45 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/nikkomiu/gentql/ent"
+	"github.com/nikkomiu/gentql/pkg/config"
+	"github.com/spf13/cobra"
+)
+
+var seedCmd = &cobra.Command{
+	Use:   "seed",
+	Short: "Seed the database with initial values",
+	RunE:  runSeed,
+}
+
+func init() {
+	rootCmd.AddCommand(seedCmd)
+}
+
+func runSeed(cmd *cobra.Command, args []string) (err error) {
+	cfg := config.GetApp()
+
+	entClient, err := ent.Open(cfg.Database.Driver, cfg.Database.URL)
+	if err != nil {
+		return
+	}
+	defer entClient.Close()
+
+	notes := []*ent.NoteCreate{
+		entClient.Note.Create().
+			SetTitle("My First Note").
+			SetBody("## My First Note Section\n\nSome content for the note. With a [link](https://blog.miu.guru) to a cool site!"),
+		entClient.Note.Create().
+			SetTitle("My Second Note").
+			SetBody("## My Other Note\n\nMore random note content...\n\n- with\n- a\n- list\n\nAll this formatting and no where to go."),
+	}
+
+	fmt.Println("Seeding notes...")
+	err = entClient.Note.CreateBulk(notes...).Exec(cmd.Context())
+
+	// create additional seeds here
+
+	return
+}
